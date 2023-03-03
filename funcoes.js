73aa3eb122fb6669f90d03fbe42289ec78fc8345
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-03 12:55:04 -0300

    Layout

--- funcoes.js
+++ funcoes.js
@@ -10,34 +10,34 @@ function somar(){
 }
 
 function subtrair(){
-    var num3, num4, resp2;
+    var num1, num2, resp1;
 
-    num3 = parseFloat(document.getElementById("num3").value);
-    num4 = parseFloat(document.getElementById("num4").value);
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
 
-    resp2 = num3 - num4;
+    resp1 = num1 - num2;
 
-    document.getElementById("resp2").value = resp2;
+    document.getElementById("resp1").value = resp1;
 }
 
 function dividir(){
-    var num5, num6, resp3;
+    var num1, num2, resp1;
 
-    num5 = parseFloat(document.getElementById("num5").value);
-    num6 = parseFloat(document.getElementById("num6").value);
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
 
-    resp3 = num5 / num6;
+    resp1 = num1 / num2;
 
-    document.getElementById("resp3").value = resp3;
+    document.getElementById("resp1").value = resp1;
 }
 
 function multiplicar(){
-    var num7, num8, resp4;
+    var num1, num2, resp1;
 
-    num7 = parseFloat(document.getElementById("num7").value);
-    num8 = parseFloat(document.getElementById("num8").value);
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
 
-    resp4 = num7 * num8;
+    resp1 = num1 * num2;
 
-    document.getElementById("resp4").value = resp4;
+    document.getElementById("resp1").value = resp1;
 }
