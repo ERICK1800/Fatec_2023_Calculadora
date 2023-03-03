c46030b7991e7392adf7bf1c37b2917221aced97
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-03 15:15:57 -0300

    Implentação de Recursos: Porcentagem e Raiz + correções

--- funcoes.js
+++ funcoes.js
@@ -41,3 +41,30 @@ function multiplicar(){
 
     document.getElementById("resp1").value = resp1;
 }
+
+function calcularPorcentagem() {
+    var num1, num2, resp1;
+
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
+
+    resp1 = (num1 * num2) / 100;
+
+    document.getElementById("resp1").value = resp1;
+}
+
+function limparCampos() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("resp1").value = "";
+}
+
+function calcularRaiz() {
+    var num1, resp1;
+
+    num1 = parseFloat(document.getElementById("num1").value);
+
+    resp1 = Math.sqrt(num1);
+
+    document.getElementById("resp1").value = resp1.toFixed(2);
+}
